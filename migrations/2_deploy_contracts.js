a4739436768cafc5f55ad10bd3862aecc7d17e27
siriussee <sirius_see@outlook.com>
2021-04-19 13:48:23 +0800

    topup supplychain when contracts are deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,11 +7,14 @@ const SupplyChain = artifacts.require("./SupplyChain.sol");
 const SupplyChainToken = artifacts.require("./SupplyChainToken.sol");
 
 module.exports = async(deployer, network, accounts) => {
-  let deployFarmerRole = await deployer.deploy(FarmerRole);
-  let deployDistributorRole = await deployer.deploy(DistributorRole);
-  let deployRetailerRole = await deployer.deploy(RetailerRole);
-  let deployConsumerRole = await deployer.deploy(ConsumerRole);
-  let deploySupplyChainToken = await deployer.deploy(SupplyChainToken, 'SupplyChainToken', 'SCT', '1000000');
-  let deploySupplyChain = await deployer.deploy(SupplyChain, deploySupplyChainToken.address, '1');
-
+  await deployer.deploy(FarmerRole);
+  await deployer.deploy(DistributorRole);
+  await deployer.deploy(RetailerRole);
+  await deployer.deploy(ConsumerRole);
+  const sct = await deployer.deploy(SupplyChainToken, 'SupplyChainToken', 'SCT', '1000000');
+  //const sct = SupplyChainToken.deployed();
+  const sc = await deployer.deploy(SupplyChain, sct.address, '1');
+  //const sc = SupplyChainToken.deployed();
+  await sct.transfer(sc.address, '100000')
+  //let randownvar = await deploySupplyChainToken.transfer(deploySupplyChain.address, '100000', {from: deployer});
 };
